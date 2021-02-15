568462846dcbc000f3c9398f010e8299a728d625
Bartosz Rudnik <rudnik49@gmail.com>
2021-02-15 12:34:51 +0100

    Product & Product Category entitties, Dao for entities

---
+++ src/main/java/ecommerce/entities/ProductCategory.java
@@ -0,0 +1,26 @@
+package ecommerce.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="product_category")
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "productCategory")
+    private Set<Product> products;
+
+}
