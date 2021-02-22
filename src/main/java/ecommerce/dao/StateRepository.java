55061ed014854373cc182dea3b85c8387aac0a1b
Bartosz Rudnik <rudnik49@gmail.com>
2021-02-22 10:36:03 +0100

    jpa classes and repositories for Countries & States

--- src/main/java/ecommerce/dao/StateRepository.java
+++ src/main/java/ecommerce/dao/StateRepository.java
@@ -1,20 +1,17 @@
 package ecommerce.dao;
 
-import ecommerce.entities.Product;
+import ecommerce.entities.State;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@CrossOrigin
 @RepositoryRestResource
-public interface ProductRepository extends JpaRepository<Product, Long> {
+@CrossOrigin
+public interface StateRepository extends JpaRepository<State, Integer> {
 
-    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable page);
-
-    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable page);
+    Page<State> findByCountryId(@RequestParam("id") int id, Pageable page);
 
 }
--- src/main/java/ecommerce/restConfiguration/MyDataRestConfig.java
+++ src/main/java/ecommerce/restConfiguration/MyDataRestConfig.java
@@ -2,14 +2,8 @@ package ecommerce.restConfiguration;
 
 import ecommerce.entities.Product;
 import ecommerce.entities.ProductCategory;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
