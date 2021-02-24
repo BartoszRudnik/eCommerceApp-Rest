5cfd958a7bf012082b0e774302ae22b5b0bf8a10
Bartosz Rudnik <rudnik49@gmail.com>
2021-02-24 11:47:33 +0100

    created entities for saving order to database

---
+++ src/main/java/ecommerce/entities/OrderItem.java
@@ -0,0 +1,34 @@
+package ecommerce.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "unit_price")
+    private double unitPrice;
+
+    private long productId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+}
